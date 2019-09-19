13170923a55e8dd1aeecb82a74d3c89e25c359cf
nikolasrieble <nikolas-rieble@gmx.de>
2019-09-19 21:26:18 +0200

    Simplify design

--- Kotlin_AndroidApp/app/src/main/java/com/nrieble/quizapp/MainActivity.kt
+++ Kotlin_AndroidApp/app/src/main/java/com/nrieble/quizapp/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.starting_screen)
 
         MobileAds.initialize(this)
